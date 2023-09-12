c560a5abd6bf96480b2c9341ea4317ad0a7f6309
Justin Lee Edwards <justin@highdefdesigns.com>
2023-09-11 22:34:06 -0500

    update users name || email || password

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,8 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -19,6 +21,8 @@ router.post('/register', register);
 // when you goto login you call login
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
