5d7a6ec1dd8eea76484fb929833f61da976606c7
Justin Lee Edwards <justin@highdefdesigns.com>
2023-09-15 17:28:49 -0500

    calculate average review rating

--- models/Review.js
+++ models/Review.js
@@ -31,7 +31,40 @@ const ReviewSchema = new mongoose.Schema({
   },
 });
 
-// this is an index that prevents a user from submitting more than one review per bootcamp
+// this is an index (MongoDB) that prevents a user from submitting more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
+
+// Static method to get avg rating and save
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+  const tobj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        // calling the $avg operator on the rating object in the model above
+        averageRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: tobj[0].averageRating,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Call getAverageCost after save
+ReviewSchema.post('save', function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+// Call getAverageCost before save
+ReviewSchema.pre('remove', function () {
+  this.constructor.getAveragerRating(this.bootcamp);
+});
 
 module.exports = mongoose.model('Review', ReviewSchema);
